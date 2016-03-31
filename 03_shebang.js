ccc54b8e26f5ead1e1107ee67cec0a6017f0b654
Jouni Kantola <jouni.kantola@dn.se>
2016-04-01 01:35:08 +0200

    NaN

--- 03_shebang.js
+++
@@ -1,3 +0,0 @@
-console.log("One more time, Ricky...");
-var shebangs = process.argv[2] + " " + process.argv[3];
-console.log(shebangs, shebangs);
--- 03_shebang.sh
+++ 03_shebang.sh
@@ -1,1 +1,4 @@
-#!/usr/bin/env node ./03_shebang.js "She bangs!"
+#!/usr/bin/env node
+console.log("One more time, Ricky...");
+console.log(Array(10).fill(Number(undefined)).join(''));
+console.log("Classic :-)");
